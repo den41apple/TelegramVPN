0f39a65a968e5877ad179af1f9cda4d17443cfb0
Андрей Сильных <den41_apple@bk.ru>
2023-08-07 14:46:34 +0300

    Добавлено приложение landing_page в Django app

--- frontend/vpn/settings.py
+++ frontend/vpn/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "landing_page.apps.LandingPageConfig",
+
 ]
 
 MIDDLEWARE = [
