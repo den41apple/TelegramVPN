4801d5ac265b2f88bdfbb1e3ec98538163979151
Андрей Сильных <den41_apple@bk.ru>
2023-08-08 14:56:20 +0300

    Добавлено приложение site_auth

--- frontend/vpn/settings.py
+++ frontend/vpn/settings.py
@@ -4,7 +4,6 @@ import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -15,7 +14,6 @@ SECRET_KEY = config.DJ_SECRET_KEY
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -30,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "site_auth.apps.SiteAuthConfig",
     "landing_page.apps.LandingPageConfig",
 ]
 
@@ -67,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "vpn.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -77,7 +75,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -97,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -108,7 +104,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
