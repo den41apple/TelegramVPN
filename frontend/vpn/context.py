74bc0f6d0f735056686a821f523b2287f143a8fb
Андрей Сильных <den41_apple@bk.ru>
2023-08-11 13:06:50 +0300

    Добавил иконку телеграм через контекст процессор

--- frontend/vpn/context.py
+++ frontend/vpn/context.py
@@ -6,7 +6,6 @@ from django.http import HttpRequest
 import config
 
 
-def add_context(request: HttpRequest) :
-    print("ОТРАБОТАЛ")
+def add_context(request: HttpRequest):
     context = {"telegram_link": config.DJ_TELEGRAM_BOT_URL}
     return context
