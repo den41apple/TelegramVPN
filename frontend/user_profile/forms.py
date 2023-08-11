49a54103d4cfb6ddf41b12ebb1ca3372df633c3c
Андрей Сильных <den41_apple@bk.ru>
2023-08-11 16:29:15 +0300

    Сделал красивым добавление устройства

--- frontend/user_profile/forms.py
+++ frontend/user_profile/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class DeviceForm(forms.Form):
-    name = forms.CharField(label="Имя устройства")
-    description = forms.CharField(label="Описание")
+    name = forms.CharField(label="Имя устройства", min_length=2)
+    description = forms.CharField(label="Описание", required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
