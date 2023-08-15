a35e38bfa15db772d7cc71e5a28ee1f0fd52d3ee
Андрей Сильных <den41_apple@bk.ru>
2023-08-15 14:24:17 +0300

    Перенес модель пользователя

--- telegram_bot/backend/db/db.py
+++ telegram_bot/backend/db/db.py
@@ -1,7 +1,7 @@
 """
 Инициализация объектов базы данных
 """
-from sqlalchemy import create_engine, MetaData, Column, Integer, String
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base
 import config
 
@@ -12,11 +12,3 @@ metadata = MetaData()
 Base = declarative_base(bind=engine, metadata=metadata)
 
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, unique=True)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
-    username = Column(String(50))
-    fz_user_id = Column(String(100), unique=True)
--- telegram_bot/backend/db/models.py
+++ telegram_bot/backend/db/models.py
@@ -1,3 +1,19 @@
 """
 Модели БД
 """
+from sqlalchemy import Column, Integer, String, Boolean
+
+from .db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer, unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    username = Column(String(50))
+    fz_user_id = Column(String(100), unique=True)
+    fz_is_admin = Column(Boolean, default=False)
+    fz_email = Column(String(100))
+    # fz_generated_password = Column(String(100))  # Первоначально сгенерированный пароль при регистрации
